98e6bc2b472bd3855401543eb999d220ba67f2f1
Akrmh Mohamedzain <akrmh90@gmail.com>
2025-01-27 13:06:10 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-const myImage = "../src/assets/175393721.jpeg"
+import myImage  from "../src/assets/175393721.jpeg"
 const App = () => {
   return (
     <div className="w-screen h-[100vh] bg-amber-300 flex gap-1.5">
